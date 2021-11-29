4ea629e92f17c951422c0e73f29ed1680566bb79
Emad Modaresi <emadmodaresi@gmail.com>
2021-11-29 11:39:07 +0330

    make Debug True for runing app in local host

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hl7(_50ashvtw^zj^%v!ta7cw^=gvf5-77*znlba@#e!ng()o_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
